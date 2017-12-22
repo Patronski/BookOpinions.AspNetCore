4a6a06a4843bb69ac7db77ba8298432ca1acc888
Doncho Patronski <doncho.patronski@riskfirst.bg>
2017-12-22 21:18:50 +0200

    Started to Moq object for controller tests.

--- BookOpinions.UnitTests/Controllers/HomeControllerTests.cs
+++ BookOpinions.UnitTests/Controllers/HomeControllerTests.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using BookOpinions.Data.Models;
+using BookOpinions.Services;
+using BookOpinions.Services.Models.Book;
 using BookOpinions.Web.Controllers;
 using BookOpinions.Web.Models.Home;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace BookOpinions.UnitTests.Controllers
 {
@@ -16,6 +23,33 @@ namespace BookOpinions.UnitTests.Controllers
             //var controller = new HomeController();
             //controller.WithCallTo(c => c.Index()).ShouldRenderDefaultView()
             //    .WithModel<IEnumerable<SimpleBookViewModel>>();
+        }
+
+        [TestMethod]
+        public async Task Index_ReturnsAViewResult_WithAListOfBrainstormSessions()
+        {
+            // Arrange
+            var mockService = new Mock<IHomeService>();
+
+            RoleManager<IdentityRole> mockRoleManager = new Mock<RoleManager<IdentityRole>>().Object;
+            UserManager<User> mockUserManager = new Mock<UserManager<User>>().Object;
+
+            mockService.Setup(homeService => homeService.GetPopularBooks(It.IsAny<int>())).Returns(Task.FromResult(GetBookWellsCollection()));
+            var controller = new HomeController(mockService.Object);
+
+            // Act
+            var result = await controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<StormSessionViewModel>>(
+                viewResult.ViewData.Model);
+            Assert.Equal(2, model.Count());
+        }
+
+        private List<BookWellsCollectionServiceModel> GetBookWellsCollection()
+        {
+            return null;
         }
     }
 }
---
+++ BookOpinions.UnitTests/Fakes/FakeUserManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BookOpinions.Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace BookOpinions.UnitTests.Fakes
+{
+    public class FakeUserManager : UserManager<User>
+    {
+        public FakeUserManager()
+            : base(new Mock<IUserStore<User>>().Object,
+                new Mock<IOptions<IdentityOptions>>().Object,
+                new Mock<IPasswordHasher<User>>().Object,
+                new IUserValidator<User>[0],
+                new IPasswordValidator<User>[0],
+                new Mock<ILookupNormalizer>().Object,
+                new Mock<IdentityErrorDescriber>().Object,
+                new Mock<IServiceProvider>().Object,
+                new Mock<ILogger<UserManager<User>>>().Object)
+        { }
+
+        public override Task<User> FindByEmailAsync(string email)
+        {
+            return Task.FromResult(new User { Email = email });
+        }
+
+        public override Task<bool> IsEmailConfirmedAsync(User user)
+        {
+            return Task.FromResult(user.Email == "test@test.com");
+        }
+
+        public override Task<string> GeneratePasswordResetTokenAsync(User user)
+        {
+            return Task.FromResult("---------------");
+        }
+    }
+}
